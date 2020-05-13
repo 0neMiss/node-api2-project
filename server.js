87588cc6564de342d20ecf7085e310984e63bcf1
Jordan <jordanshehane@gmail.com>
2020-05-12 23:12:24 -0600

    all working but the post to comments

--- server.js
+++ server.js
@@ -1,82 +1,30 @@
 const express = require('express');
-
+const dataRouter = require('./data/datarouter.js');
 const posts = require('./data/db.js');
-
-const shortid = require('shortid');
-
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', dataRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Lets gooo</h>`);
 });
 
-server.get('/api/posts', (req, res) => {
-  posts.find(req.query)
-  .then(posts => {
-    res.status(200).json(posts);
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the posts',
-    });
-  });
-});
-
-server.get('/api/posts/:id', (req, res) => {
-  posts.findById(req.params.id)
-  .then(post => {
-
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({
-      message: 'Error retrieving the post',
-    });
-  });
-});
-
 server.post('/api/posts', (req, res) => {
+  console.log(req.body);
   posts.insert(req.body)
   .then(post => {
-
+    console.log(post);
+    if(req.body.title && req.body.contents){
+      res.status(201).json(post);
+    }
+    else{
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
   })
   .catch(error => {
     console.log(error);
-    res.status(500).json({
-      message: 'Error adding the post',
-    });
+    res.status(500).json({ error: "There was an error while saving the comment to the database" });
   });
 });
-
-server.delete('/api/posts/:id', (req, res) => {
-  posts.remove(req.params.id)
-  .then(count => {
-
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).json({
-      message: 'Error removing the post',
-    });
-  });
-});
-
-server.put('/api/posts/:id', (req, res) => {
-  const changes = req.body;
-  posts.update(req.params.id, changes)
-  .then(post => {
-    
-  })
-  .catch(error => {
-    // log error to database
-    console.log(error);
-    res.status(500).json({
-      message: 'Error updating the post',
-    });
-  });
-});
-
 module.exports = server;
